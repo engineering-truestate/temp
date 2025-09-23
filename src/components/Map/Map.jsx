aac906ac84d03c48f3c2f5f26198c9d4ea3435fd
kulA8520 <kulasekhar@truestate.in>
2025-09-23 11:18:43 +0530

    truestate changes

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -40,6 +40,7 @@ import { logEvent } from "firebase/analytics";
 import { analytics } from "../../firebase";
 import Footer from "../../landing/pages/home/Footer";
 import NoPropertiesFound from "../NoPropertiesFound/NoPropertiesFound.jsx";
+import algoliaservice from "../../services/algoliaService";
 
 const defaultCenter = [12.9021306, 77.5976507];
 
@@ -240,19 +241,46 @@ const MapComponent = ({
 
   const { hits, results } = useHits();
   const isReduxLoading = useSelector(selectLoader);
+  // const [updatedProjects, setUpdatedProjects] = useState([]);
 
-  // Memoize updated projects to prevent unnecessary recalculations
-  const updatedProjects = useMemo(() => {
+  // // Memoize updated projects to prevent unnecessary recalculations
+  // const updatedProjects = useMemo(() => {
+  //   if (mapType === "all") {
+  //     const filtered = hits.filter(
+  //       (project) =>
+  //         parseFloat(project?.locationAnalysis?.lat) &&
+  //         parseFloat(project?.locationAnalysis?.long)
+  //     );
+  //     return filtered.map((proj) => ({ ...proj, id: proj.objectID }));
+  //   }
+  //   return projects;
+  // }, [mapType, hits, projects]);
+  const [updatedProjects, setUpdatedProjects] = useState([]);
+
+useEffect(() => {
+  async function fetchProjects() {
     if (mapType === "all") {
-      const filtered = hits.filter(
+      const res = await algoliaservice.getMapProperties({
+        isAuthenticated,             
+        hitsPerPage: 1000,                
+        page: 0                          
+      });
+
+      const filtered = res?.hits?.filter(
         (project) =>
           parseFloat(project?.locationAnalysis?.lat) &&
           parseFloat(project?.locationAnalysis?.long)
-      );
-      return filtered.map((proj) => ({ ...proj, id: proj.objectID }));
+      ) || [];
+
+      setUpdatedProjects(filtered.map((proj) => ({ ...proj, id: proj.objectID })));
+    } else {
+      setUpdatedProjects(projects);
     }
-    return projects;
-  }, [mapType, hits, projects]);
+  }
+
+  fetchProjects();
+}, [mapType, projects]);
+
 
   const { isAuthenticated } = useSelector((state) => state.auth);
   const { loading } = useSelector((state) => state.projectsState);
@@ -723,13 +751,13 @@ const MapComponent = ({
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
 
-            {mapType === "all" && micromarketsData && (
+            {/* {mapType === "all" && micromarketsData && (
               <GeoJSON
                 data={micromarketsData}
                 style={micromarketsStyle}
                 onEachFeature={onEachFeature}
               />
-            )}
+            )} */}
 
             <ClusteredMarkers markers={updatedProjects} />
             <Markers markers={metroData} />
